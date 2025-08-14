c19f583ead0928651244465e9d535d3fbce1d64e
원정우 <rjstn155@naver.com>
2025-08-14 22:00:57 +0900

    Initial commit

---
+++ Assets/02. Script/Manager/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 클리어 했는지
+
+    // 시간 관련
+}
---
+++ Assets/98. ScriptableObj/MiniGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MiniGame : ScriptableObject
+{
+    public string name;
+
+}
