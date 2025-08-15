92c16ef69878b0bca22aa8ceeae1dcca3d733651
charliebrown1994 <leesungyul@outlook.com>
2025-08-15 16:57:57 +0900

    [Feat] 이승율 : 이동과 달리기 그리고 애니메이션 추가

--- Assets/02. Script/Player_LSY/PlayerController.cs
+++ Assets/02. Script/Player_LSY/PlayerController.cs
@@ -1,8 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [Header("Moverment")]
+    public float moveSpeed;
+    public float runSpeed;
+    public float jumpPower;
+    private Vector2 curMovementInput;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")]
+    private Vector2 mouseDelta;
+
+    private Rigidbody _rigidbody;
+    private Animator animator;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
+        _rigidbody.velocity = dir;
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            animator.SetBool("isWalking", true);
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            animator.SetBool("isWalking", false);
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {   
+            moveSpeed += runSpeed;
+            animator.SetBool("isRunning", true);
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            moveSpeed -= runSpeed;
+            animator.SetBool("isRunning", false); 
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
 }
