5166670f573e1dac193c80cbe90f3dc9c1ce2dc4
Nokddu <sksksksk103@naver.com>
2025-08-16 17:28:32 +0900

    [Feat] KYM : 게임매니저, 스크립터블 제작

--- Assets/02. Script/Manager/GameManager.cs
+++ Assets/02. Script/Manager/GameManager.cs
@@ -6,5 +6,18 @@ public class GameManager : MonoBehaviour
 {
     // 클리어 했는지
 
+    public List<bool> GameCleared = new List<bool>();
+
+    public void isGameCleared(bool clear, int index)
+    {
+        if (index > GameCleared.Count - 1) return;
+        GameCleared[index] = clear;
+    }
+
+    
+
+
     // 시간 관련
+
+
 }
