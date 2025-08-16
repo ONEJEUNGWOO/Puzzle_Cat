3c14df10dc2459ade342f7aa3158cfed8eb16c03
Nokddu <sksksksk103@naver.com>
2025-08-16 17:57:15 +0900

    [Feat] KYM : 전체적인 데이터

---
+++ Assets/02. Script/Data/MiniGameData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteract
+{
+    public void SetGame(int number);
+}
+
+public class MiniGameData : MonoBehaviour
+{
+    public MiniGame games;
+
+
+}
