291d39148e7d44880f535b4063a4912b2e20f8b7
charliebrown1994 <leesungyul@outlook.com>
2025-08-19 12:10:11 +0900

    [Feat] 상호작용 숨기기

--- Assets/02. Script/Player_LSY/InteractableObject.cs
+++ Assets/02. Script/Player_LSY/InteractableObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    public string InteractionText; // 상호작용할 때 표시될 텍스트
+    public string InteractionText; // 감지됬을 때 표시될 텍스트
     public virtual void Interact()
     // 상호작용을 처리하는 함수
     // virtual 키워드를 사용하여 상속받은 클래스에서 오버라이드할 수 있도록 합니다.
